bb4b7382ecf6bb805c76ac6d2e517abf8c1d22ff
Kirillgutsalo <kirya.sever11@gmail.com>
2021-12-24 01:41:27 +0200

    adds commit with update homework#23

--- src/telran/util/time/NextFriday13Adjuster.java
+++ src/telran/util/time/NextFriday13Adjuster.java
@@ -12,15 +12,10 @@ public class NextFriday13Adjuster implements TemporalAdjuster {
 	@Override
 	public Temporal adjustInto(Temporal temporal) {
 		temporal = temporal.with(TemporalAdjusters.next(DayOfWeek.FRIDAY));
-		while (!isFriday13(temporal)) {
+		while (temporal.get(ChronoField.DAY_OF_MONTH) != 13) {
 			temporal = temporal.plus(1, ChronoUnit.WEEKS);
 		}
 		return temporal;
-	}
-
-	private boolean isFriday13(Temporal temporal) {
-		
-		return temporal.get(ChronoField.DAY_OF_MONTH) == 13;
 	}
 
 }
